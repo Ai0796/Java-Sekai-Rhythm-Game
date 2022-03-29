74244453794226dbd4424b30e890f62f0747a378
Alden Mo <mo.alden25@gmail.com>
2022-03-29 16:33:40 -0400

    Added initialization to RhythmScreen

--- src/main/gui/RhythmScreen.java
+++ src/main/gui/RhythmScreen.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 
 import main.Conductor;
 import main.FrameController;
+import main.Lane;
 import main.settings.Config;
 
 public class RhythmScreen implements InnerBaseGui{
@@ -11,6 +12,9 @@ public class RhythmScreen implements InnerBaseGui{
     private JFrame frame;
     private Config config;
     private FrameController frameController;
+
+    private static final double laneWidthRatio = 1.0 / 10.0;
+    private Lane[] lanes;
     
     public RhythmScreen(JFrame frame, Config config, FrameController frameController){
         
@@ -18,6 +22,12 @@ public class RhythmScreen implements InnerBaseGui{
         this.frame = frame;
         this.frameController = frameController;
 
+        lanes = new Lane[config.getLanes()];
+
+        for(int i = 0; i < config.getLanes(); i++){
+            Lane lane = new Lane();
+            lanes[i] = lane;
+        }
     }
 
     public void startGame(){
